0ddff7fbb270ad53d15b640328039c554175cc12
GiovanYCringe <gio.pikagrossa@gmail.com>
2022-08-08 16:29:42 +0000

    Revert "deen: Aicipify"
    This reverts commit 107318d40a23df711ac6e938938e93dbdf4d358e.

--- device.mk
+++ device.mk
@@ -33,7 +33,8 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     AvoidAppsInCutoutOverlay \

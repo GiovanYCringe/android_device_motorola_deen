c42e9c1e8378b9f6eb78cf71f1ede5142c563480
GiovanYCringe <gio.pikagrossa@gmail.com>
2022-08-22 11:55:09 +0000

    deen: enable fastbootd

--- device.mk
+++ device.mk
@@ -112,6 +112,10 @@ TARGET_EXCLUDES_AUDIOFX := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \

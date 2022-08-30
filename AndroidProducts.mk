a64f5990b8e36cbccdb5853a232b75d753bea2c1
GiovanYCringe <gio.pikagrossa@gmail.com>
2022-08-30 23:17:35 +0000

    Revert "deen: Enable verity" * deen: lineage > aosp
    This reverts commit 8a89ca9c460dd16be2c9c0f3b8285113e1661b3f.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_deen.mk
+    $(LOCAL_DIR)/aosp_deen.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_deen-user \
-    lineage_deen-userdebug \
-    lineage_deen-eng
+    aosp_deen-user \
+    aosp_deen-userdebug \
+    aosp_deen-eng

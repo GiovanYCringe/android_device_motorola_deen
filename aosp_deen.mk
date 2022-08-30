2fc43730fb3abff8cd06ec962cfb4be9845632c3
GiovanYCringe <gio.pikagrossa@gmail.com>
2022-08-30 23:20:36 +0000

    deen: removed build_fingerprint

--- aosp_deen.mk
+++ aosp_deen.mk
@@ -39,4 +39,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=deen \
     PRIVATE_BUILD_DESC="deen-user 10 QPKS30.54-22-27 92b8a release-keys"
 
-BUILD_FINGERPRINT := motorola/deen/deen_sprout:10/QPKS30.54-22-27/92b8a:user/release-keys

60d9ce8d7ef75e26b2d3b37a06bd43b26a739c44
GiovanYCringe <gio.pikagrossa@gmail.com>
2022-09-02 03:25:48 +0000

    deen: lineage to banana

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_deen.mk
+    $(LOCAL_DIR)/banana_deen.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_deen-user \
-    lineage_deen-userdebug \
-    lineage_deen-eng
+    banana_deen-user \
+    banana_deen-userdebug \
+    banana_deen-eng

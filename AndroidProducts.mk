f8ee7af02622648f18b3cdebf3beb8701da38050
Giovany <106202775+GiovanYCringe@users.noreply.github.com>
2022-09-02 22:59:51 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_deen.mk
+    $(LOCAL_DIR)/havoc_deen.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_deen-user \
-    awaken_deen-userdebug \
-    awaken_deen-eng
+    havoc_deen-user \
+    havoc_deen-userdebug \
+    havoc_deen-eng
